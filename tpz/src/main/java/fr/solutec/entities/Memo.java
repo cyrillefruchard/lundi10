a8b5d09ec220bb04a1c832b3823beca26df7041b
lacyohann <yohann.lac.perso@gmail.com>
2022-01-10 16:35:27 +0100

    Memo

--- tpz/src/main/java/fr/solutec/entities/Memo.java
+++ tpz/src/main/java/fr/solutec/entities/Memo.java
@@ -1,5 +1,22 @@
 package fr.solutec.entities;
 
-public class Memo {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor @Data
+public class Memo {
+	@Id @GeneratedValue
+	private Long id;
+	private String contenu;
+	@ManyToOne
+	private Utilisateur expediteur;
+	@ManyToOne
+	private Utilisateur destinataire;
 }
