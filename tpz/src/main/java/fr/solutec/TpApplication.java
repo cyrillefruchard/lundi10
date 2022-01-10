9adc7dd515552b9f2bb7c3309a97377e6d8cfcb0
cyrillefruchard <cyrille.fruchard@yahoo.fr>
2022-01-10 16:44:30 +0100

    main

--- tpz/src/main/java/fr/solutec/TpApplication.java
+++ tpz/src/main/java/fr/solutec/TpApplication.java
@@ -1,14 +1,32 @@
 package fr.solutec;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.solutec.entities.Memo;
+import fr.solutec.entities.Utilisateur;
+
 @SpringBootApplication
-public class TpApplication {
+public class TpApplication implements CommandLineRunner {
+	@Autowired
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpApplication.class, args);
-		int a = 2;
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("lancement des applications perso");
+
+		Utilisateur u1 = new Utilisateur(null, "Marc", "Dupondt", "Marco", "azertyuiop");
+		Utilisateur u2 = new Utilisateur(null, "James", "Dupondt", "Thor", "aa");
+
+		Memo m1 = new Memo(null, "Salut ça va", u1, u2);
+		Memo m2 = new Memo(null, "ok nickel", u2, u1);
+
 	}
 
 }
