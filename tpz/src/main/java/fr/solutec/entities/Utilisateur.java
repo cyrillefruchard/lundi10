dc1e921714fc02e10ee62fa19c2ce6ff7cab5795
cyrillefruchard <cyrille.fruchard@yahoo.fr>
2022-01-10 16:29:31 +0100

    classe utilisateur

--- tpz/src/main/java/fr/solutec/entities/Utilisateur.java
+++ tpz/src/main/java/fr/solutec/entities/Utilisateur.java
@@ -1,6 +1,27 @@
 package fr.solutec.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Utilisateur {
-	
+
+	@Id
+	@GeneratedValue
+
+	private long id;
+	private String prenom;
+	private String nom;
+	private String login;
+	private String mdp;
 
 }
