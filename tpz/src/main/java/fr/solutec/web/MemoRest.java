bfae28565491e316207dddd22eaaa51a7ffbd06e
lacyohann <yohann.lac.perso@gmail.com>
2022-01-10 16:48:55 +0100

    momorest

--- tpz/src/main/java/fr/solutec/web/MemoRest.java
+++ tpz/src/main/java/fr/solutec/web/MemoRest.java
@@ -1,11 +1,14 @@
 package fr.solutec.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.repositories.MemoRepository;
+
 @RestController @CrossOrigin("*")
 public class MemoRest {
-	
+	@Autowired
 	private MemoRepository memoRepo;
 	
 }
