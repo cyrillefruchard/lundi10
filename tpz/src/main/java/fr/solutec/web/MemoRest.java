4e54fdd9cefe75678fb0c182ea05931ff2f1dcd4
lacyohann <yohann.lac.perso@gmail.com>
2022-01-10 16:40:40 +0100

    rest

--- tpz/src/main/java/fr/solutec/web/MemoRest.java
+++ tpz/src/main/java/fr/solutec/web/MemoRest.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController @CrossOrigin("*")
-public class UserRest {
-
+public class MemoRest {
+	
 }
