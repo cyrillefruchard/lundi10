77fce762815a2d15e5e22c18898bf30dba682052
lacyohann <yohann.lac.perso@gmail.com>
2022-01-11 10:22:15 +0100

    memo find

--- tpz/src/main/java/fr/solutec/repositories/MemoRepository.java
+++ tpz/src/main/java/fr/solutec/repositories/MemoRepository.java
@@ -1,9 +1,14 @@
 package fr.solutec.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.Memo;
+import fr.solutec.entities.Utilisateur;
 
 public interface MemoRepository extends CrudRepository<Memo, Long>{
+	public List<Memo> findByExpediteur(Optional<Utilisateur> expediteur);
 
 }
--- tpz/src/main/java/fr/solutec/web/MemoRest.java
+++ tpz/src/main/java/fr/solutec/web/MemoRest.java
@@ -1,14 +1,33 @@
 package fr.solutec.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.entities.Memo;
+import fr.solutec.entities.Utilisateur;
 import fr.solutec.repositories.MemoRepository;
+import fr.solutec.repositories.UserRepository;
 
 @RestController @CrossOrigin("*")
 public class MemoRest {
 	@Autowired
 	private MemoRepository memoRepo;
+	@Autowired
+	private UserRepository userRepo;
 	
+	@GetMapping("/memo/expediteur/{id}")
+	public Iterable<Memo> getMemoUser(@PathVariable Long id){
+		Optional<Utilisateur> expediteur = userRepo.findById(id);
+		return memoRepo.findByExpediteur(expediteur);
+	}
+	
+	@GetMapping("memo")
+	public Iterable<Memo> getAllMemo(){
+		return memoRepo.findAll();
+	}
 }
