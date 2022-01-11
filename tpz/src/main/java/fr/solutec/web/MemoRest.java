6d4e7187a0ca8b64eee56d23beedeb4dfd6057e4
lacyohann <yohann.lac.perso@gmail.com>
2022-01-11 10:50:00 +0100

    m

--- tpz/src/main/java/fr/solutec/web/MemoRest.java
+++ tpz/src/main/java/fr/solutec/web/MemoRest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Memo;
@@ -30,4 +32,10 @@ public class MemoRest {
 	public Iterable<Memo> getAllMemo(){
 		return memoRepo.findAll();
 	}
+	
+	@PostMapping("memo")
+	public Memo savePublicMemo(@RequestBody Memo m) {
+		return memoRepo.save(m);
+	}
+	
 }
