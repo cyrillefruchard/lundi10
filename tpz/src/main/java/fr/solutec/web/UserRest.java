aebdb33ef49aa026e503b6344d30e55d1bdf27f2
lacyohann <yohann.lac.perso@gmail.com>
2022-01-11 09:06:35 +0100

    x

--- tpz/src/main/java/fr/solutec/web/UserRest.java
+++ tpz/src/main/java/fr/solutec/web/UserRest.java
@@ -1,9 +1,13 @@
 package fr.solutec.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.repositories.UserRepository;
+
 @RestController @CrossOrigin("*")
 public class UserRest {
-
+	@Autowired
+	private UserRepository userRepo;
 }
