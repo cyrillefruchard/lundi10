e14c11562d3a7f4a1cfd0286664e4c071037fe6a
cyrillefruchard <cyrille.fruchard@yahoo.fr>
2022-01-11 10:18:51 +0100

    1ere fonction

--- tpz/src/main/java/fr/solutec/repositories/UserRepository.java
+++ tpz/src/main/java/fr/solutec/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package fr.solutec.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.Utilisateur;
 
-public interface UserRepository extends CrudRepository<Utilisateur, Long>{
+public interface UserRepository extends CrudRepository<Utilisateur, Long> {
+	@Query(value = "SELECT u FROM Utilisateur u WHERE u.login = ?1 AND u.mdp= ?2")
+	public List<Utilisateur> findByLoginAndPassword(String login, String mdp);
 
 }
--- tpz/src/main/java/fr/solutec/web/UserRest.java
+++ tpz/src/main/java/fr/solutec/web/UserRest.java
@@ -1,13 +1,31 @@
 package fr.solutec.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.entities.Utilisateur;
 import fr.solutec.repositories.UserRepository;
 
-@RestController @CrossOrigin("*")
+@RestController
+@CrossOrigin("*")
 public class UserRest {
 	@Autowired
 	private UserRepository userRepo;
+
+	@GetMapping("user")
+	public Iterable<Utilisateur> getAllPersons() {
+		return userRepo.findAll();
+	}
+
+	@PostMapping("user/login")
+	public List<Utilisateur> savePerson(@RequestBody Utilisateur p) {
+		return userRepo.findByLoginAndPassword(p.getLogin(), p.getMdp());
+	}
+
 }
